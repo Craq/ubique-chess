1d911b96a672044bfe183540c8706b668032dfe4
Craq <protion86@gmail.com>
2018-05-01 13:16:32 +0300

    particles

--- Backend/pages.js
+++ Backend/pages.js
@@ -1,6 +1,8 @@
-/**
- * Created by chaika on 09.02.16.
- */
+exports.mainPage = function(req, res){
+    res.render('mainPage', {
+        pageTitle: 'chess.ai'
+    });
+};
 exports.choice = function(req, res){
     res.render('chooseChess', {
         pageTitle: 'Choice'
